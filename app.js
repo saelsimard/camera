f62db292e64abfb4114dba43e06c86413396a6f8
Saël Simard <sael@kommon.io>
2019-04-30 18:02:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "back" }, audio: false };
+var constraints = { video: { facingMode: "environment" }, audio: false };
 var track = null;
 
 // Define constants
